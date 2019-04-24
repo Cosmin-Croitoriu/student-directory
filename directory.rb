762c572a4f2359af478dc7a0df6f9bef7f0d834b
Cosmin <cosmin.c.croitoriu@gmail.com>
2019-04-24 16:30:10 +0100

    adds array of hashes

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 students = [
-{name: "Dr. Hannibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "Steven Wolfe", cohort: :november},
-{name: "The Jocker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november}
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "Steven Wolfe", cohort: :november},
+  {name: "The Jocker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 def print_header
   puts "The students of Villains Academy"
@@ -19,7 +19,7 @@ end
 
 def print(students)
   students.each do |student|
-    puts "#{student[0]}: #{student[1]} cohort"
+    puts "#{student[:name]}: #{student[:cohort]} cohort"
   end
 end
 
