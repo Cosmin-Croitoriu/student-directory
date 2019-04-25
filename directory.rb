ad606dee7aaa2d59a8e2f36666611d84f3dd7606
Cosmin <cosmin.c.croitoriu@gmail.com>
2019-04-25 13:32:24 +0100

    adds file save and fixes mistakes

--- directory.rb
+++ directory.rb
@@ -22,7 +22,7 @@ def input_students
   name = gets.chomp
   while !name.empty? do
     @students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
 end
@@ -40,6 +40,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -50,6 +52,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students file"
   puts "9. Exit"
 end
 
@@ -70,6 +73,18 @@ def print_students_list
   end
 end
 
-def print_footer(@students)
+def print_footer
   puts "Overall we have #{@students.count} great students"
 end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(',')
+    file.puts csv_line
+  end
+  file.close
+end
+
+interactive_menu
